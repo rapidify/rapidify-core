dd48e9fe8ae456cda0792b8f5f610436c3aa711f
Aakash Verma <aakashvermaee@gmail.com>
2019-11-10 01:46:20 +0530

    initial logger work

--- config/database.js
+++ config/database.js
@@ -1,31 +1,33 @@
-const mongoose = require('mongoose');
-const Config = require('./config');
+const mongoose = require("mongoose");
+const chalkGreen = require("chalk").green;
+
+const Config = require("./config");
 
 class DbConnection {
-    constructor() {
-        this.config = new Config();
-        this.MONGO_DB = this.config.MONGO_DB;
-        this.MONGO_DB_USER = this.config.MONGO_DB_USER;
-        this.MONGO_DB_URL = this.config.MONGO_DB_URL;
-        this.MONGO_DB_PASSWORD = this.config.MONGO_DB_PASSWORD;
+  constructor() {
+    this.config = new Config();
+    this.MONGO_DB = this.config.MONGO_DB;
+    this.MONGO_DB_USER = this.config.MONGO_DB_USER;
+    this.MONGO_DB_URL = this.config.MONGO_DB_URL;
+    this.MONGO_DB_PASSWORD = this.config.MONGO_DB_PASSWORD;
+  }
+
+  async connect() {
+    try {
+      mongoose.set("user", this.MONGO_DB_USER);
+      mongoose.set("password", this.MONGO_DB_PASSWORD);
+
+      mongoose.set("useNewUrlParser", true);
+      mongoose.set("useFindAndModify", false);
+      mongoose.set("useUnifiedTopology", true);
+
+      await mongoose.connect(this.MONGO_DB_URL);
+
+      console.log(chalkGreen("Connected to Mongo"));
+    } catch (err) {
+      throw err;
     }
-
-    async connect() {
-        try {
-            mongoose.set("user", this.MONGO_DB_USER);
-            mongoose.set("password", this.MONGO_DB_PASSWORD);
-
-            mongoose.set("useNewUrlParser", true);
-            mongoose.set("useFindAndModify", false);
-            mongoose.set("useUnifiedTopology", true);
-
-            await mongoose.connect(this.MONGO_DB_URL);
-
-            console.log('Connected to Mongo');
-        } catch (err) {
-            throw err;
-        }
-    }
+  }
 }
 
 module.exports = DbConnection;
